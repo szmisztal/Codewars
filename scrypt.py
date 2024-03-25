a98a5ec5654180be190212c8b4b27ab9906e3574
szmisztal <sz.misztal@gmail.com>
2024-03-25 17:46:36 +0100

    Commit by script

--- scrypt.py
+++ scrypt.py
@@ -4,7 +4,7 @@ def git_commit_push(message):
     try:
         subprocess.run(["git", "add", "."], check = True)
         subprocess.run(["git", "commit", "-m", message], check = True)
-        subprocess.run(["git", "push origin master"], check = True)
+        subprocess.run(["git", "push"], check = True)
         print("Successfully sending commits on GH")
     except subprocess.CalledProcessError as e:
         print(f"Error: {e}")
