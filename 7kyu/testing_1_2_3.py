a5323fe504414cf9c6edcc7a829bae41c21a52e0
szmisztal <sz.misztal@gmail.com>
2024-03-27 12:08:25 +0100

    Automatic commit by my script

---
+++ 7kyu/testing_1_2_3.py
@@ -0,0 +1,14 @@
+"""Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+Examples: (Input --> Output)
+
+[] --> []
+["a", "b", "c"] --> ["1: a", "2: b", "3: c"]"""
+
+
+def number(lines):
+    return [f"{i}: {lines[i - 1]}" for i in range(1, len(lines) + 1)]
