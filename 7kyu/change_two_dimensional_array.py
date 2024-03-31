a4891b870caba6ab1449d7faf336a5c5652c758a
szmisztal <sz.misztal@gmail.com>
2024-03-31 10:25:16 +0200

    Automatic commit by my script

---
+++ 7kyu/change_two_dimensional_array.py
@@ -0,0 +1,42 @@
+"""Function receive a two-dimensional square array of random integers. On the main diagonal, all the negative integers must be changed to 0, while the others must be changed to 1 (Note: 0 is considered non-negative, here).
+
+(You can mutate the input if you want, but it is a better practice to not mutate the input)
+
+Example:
+
+Input array
+
+[
+  [-1,  4, -5, -9,  3 ],
+  [ 6, -4, -7,  4, -5 ],
+  [ 3,  5,  0, -9, -1 ],
+  [ 1,  5, -7, -8, -9 ],
+  [-3,  2,  1, -5,  6 ]
+]
+Output array
+
+[
+  [ 0,  4, -5, -9,  3 ],
+  [ 6,  0, -7,  4, -5 ],
+  [ 3,  5,  1, -9, -1 ],
+  [ 1,  5, -7,  0, -9 ],
+  [-3,  2,  1, -5,  1 ]
+]"""
+
+
+def matrix(array):
+    for i in range(len(array)):
+        if array[i][i] < 0:
+            array[i][i] =  0
+        else:
+            array[i][i] = 1
+    return array
+
+
+print(matrix([
+  [-1,  4, -5, -9,  3 ],
+  [ 6, -4, -7,  4, -5 ],
+  [ 3,  5,  0, -9, -1 ],
+  [ 1,  5, -7, -8, -9 ],
+  [-3,  2,  1, -5,  6 ]
+]))
