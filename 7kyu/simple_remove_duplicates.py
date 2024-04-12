ad0d9007153e215ddcbdefda661dc67515cf006f
szmisztal <sz.misztal@gmail.com>
2024-04-12 13:57:48 +0200

    New tasks automatically added by my script

---
+++ 7kyu/simple_remove_duplicates.py
@@ -0,0 +1,23 @@
+"""Remove the duplicates from a list of integers, keeping the last ( rightmost ) occurrence of each element.
+
+Example:
+For input: [3, 4, 4, 3, 6, 3]
+
+remove the 3 at index 0
+remove the 4 at index 1
+remove the 3 at index 3
+Expected output: [4, 6, 3]
+
+More examples can be found in the test cases.
+
+Good luck!"""
+
+
+def solve(arr):
+    result = []
+    for i in arr[::-1]:
+        if i not in result:
+            result.append(i)
+    return result[::-1]
+
+
