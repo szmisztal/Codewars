c09df91f8a9c959f35c78cb5b8a1e8cb29a6db80
szmisztal <sz.misztal@gmail.com>
2024-06-06 16:32:17 +0200

    New tasks automatically added by my script

---
+++ 7kyu/simple_fun_176_reverse_letter.py
@@ -0,0 +1,17 @@
+"""Task
+Given a string str, reverse it and omit all non-alphabetic characters.
+
+Example
+For str = "krishan", the output should be "nahsirk".
+
+For str = "ultr53o?n", the output should be "nortlu".
+
+Input/Output
+[input] string str
+A string consists of lowercase latin letters, digits and symbols.
+
+[output] a string"""
+
+
+def reverse_letter(st):
+    return "".join(s for s in st if s.isalpha())[::-1]
