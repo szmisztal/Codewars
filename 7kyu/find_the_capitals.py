5a474fdf2ce4fe24b3e03a64164088506bc39ad6
szmisztal <sz.misztal@gmail.com>
2024-06-30 11:31:56 +0200

    New tasks automatically added by my script

---
+++ 7kyu/find_the_capitals.py
@@ -0,0 +1,12 @@
+"""Instructions
+Write a function that takes a single non-empty string of only lowercase and uppercase ascii letters (word) as its argument, and returns an ordered list containing the indices of all capital (uppercase) letters in the string.
+
+Example (Input --> Output)
+"CodEWaRs" --> [0,3,4,6]"""
+
+
+def capitals(word):
+    return [index for index, letter in enumerate(word) if letter.isupper()]
+
+
+
