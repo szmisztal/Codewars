1f17144471cad891822fcab61dff912f5506ecae
szmisztal <sz.misztal@gmail.com>
2024-07-12 12:59:23 +0200

    New tasks automatically added by my script

---
+++ 8kyu/exclamation_marks_series_2_remove_all_exclamation_marks_from_the_end_of_sentence.py
@@ -0,0 +1,14 @@
+"""Description:
+Remove all exclamation marks from the end of sentence.
+
+Examples
+"Hi!"     ---> "Hi"
+"Hi!!!"   ---> "Hi"
+"!Hi"     ---> "!Hi"
+"!Hi!"    ---> "!Hi"
+"Hi! Hi!" ---> "Hi! Hi"
+"Hi"      ---> "Hi"""
+
+
+def remove(st):
+    return st.rstrip("!")
