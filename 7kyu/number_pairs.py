4a3f96c6b99fa14c32479a7774e7ae33ba375315
szmisztal <sz.misztal@gmail.com>
2024-07-14 11:55:44 +0200

    New tasks automatically added by my script

---
+++ 7kyu/number_pairs.py
@@ -0,0 +1,13 @@
+"""In this kata the aim is to compare each pair of integers from two arrays, and return a new array of large numbers.
+
+Note: Both arrays have the same dimensions.
+
+Example:
+arr1 = [13, 64, 15, 17, 88]
+arr2 = [23, 14, 53, 17, 80]
+get_larger_numbers(arr1, arr2) == [23, 64, 53, 17, 88]"""
+
+
+def get_larger_numbers(a, b):
+    return [max(pair) for pair in zip(a, b)]
+
