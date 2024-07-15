9db481a2cab65e6dc5b5623483d7c72828eecd21
szmisztal <sz.misztal@gmail.com>
2024-07-15 12:13:01 +0200

    New tasks automatically added by my script

---
+++ 7kyu/flatten_and_sort_an_array.py
@@ -0,0 +1,17 @@
+"""Challenge:
+
+Given a two-dimensional array of integers, return the flattened version of the array with all the integers in the sorted (ascending) order.
+
+Example:
+
+Given [[3, 2, 1], [4, 6, 5], [], [9, 7, 8]], your function should return [1, 2, 3, 4, 5, 6, 7, 8, 9]."""
+
+
+def flatten_and_sort(array):
+    result = []
+    for i in array:
+        result.extend(i)
+    return sorted(result)
+
+
+print(flatten_and_sort([[1, 3, 5], [100], [2, 4, 6]]))
